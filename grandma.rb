bd155fb1d6cdf4d1c1134b9bb2d5d495f00ec6ec
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-14 00:21:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,11 @@
 def speak_to_grandma(phrase)
 if phrase== "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif phrase== "I LOVE YOU GRANDMA!".upcase
+elsif phrase== phrase.upcase
   return "NO, NOT SINCE 1938!"
 else "i love you grandma!".capitalize
   return "HUH?! SPEAK UP, SONNY!"
 end 
 end 
+# phrase= WHAT DID YOU EAT TODAY?
+# phrase= 
